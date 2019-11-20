2cde4dc9549b7797541a381fdbe0993634946d46
RomainCostes <44799984+RomainCostes@users.noreply.github.com>
2019-11-20 14:40:30 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,75 @@
+
+<header>
+  <nav class="navbar">
+    <div class="logo">
+      EPSILON
+    </div>
+    <div class="sitenavigation">
+      <span class="menu-icon">
+  <a href="#" class="menu example5"><span></span></a>
+      <div id="hamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      </span>
+      <ul>
+        <li class="nav-dropdown"><a href="#">NOUVEAUT&Eacute;S</a>
+          <ul>
+            <li><a href="#">Hot Sellers</a></li>
+            <li><a href="#">On Sale</a></li>
+            <li class="nav-dropdown"><a href="#">Men's</a>
+              <ul>
+                <li><a href="#">T-Shirts</a></li>
+                <li><a href="#">Pants</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Clearance</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-dropdown"><a href="#">HOMME</a>
+          <ul>
+            <li><a href="#">Hot Sellers</a></li>
+            <li><a href="#">On Sale</a></li>
+            <li class="nav-dropdown"><a href="#">Women's</a>
+              <ul>
+                <li><a href="#">Dresses</a></li>
+                <li class="nav-dropdown"><a href="#">Shoes</a>
+                  <ul>
+                    <li>
+                      <li><a href="#">High Heels</a></li>
+                      <li><a href="#">Tennis Shoes</a></li>
+                      <li><a href="#">Flip Flops</a></li>
+                  </ul>
+                  </li>
+              </ul>
+              </li>
+              <li><a href="#">Forth Sub-nav item</a></li>
+          </ul>
+          </li>
+
+          <li class="nav-dropdown"><a href="#">FEMME</a>
+            <ul>
+              <li><a href="#">Nav item</a></li>
+              <li class="nav-dropdown"><a href="#">Nav item</a>
+                <ul>
+                  <li><a href="#">Nav item</a></li>
+
+                </ul>
+              </li>
+              <li class="nav-dropdown"><a href="#">Third Sub-nav item</a>
+                <ul>
+                  <li><a href="#">Third level nav item</a></li>
+                </ul>
+              </li>
+
+              <li><a href="#">Forth Sub-nav item</a></li>
+            </ul>
+          </li>
+
+          <li><a href="#">COLLECTIONS</a></li>
+      </ul>
+    </div>
+  </nav>
+</header>
